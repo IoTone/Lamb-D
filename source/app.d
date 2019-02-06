00b2e8aa2fbe31ec5ee5ec4269e9caff29f60b67
David J. Kordsmeier <dkords@gmail.com>
2019-02-05 23:20:43 -0800

    refs #1 Add missing gitignore, add new layer stubs, dub.json is library type

--- source/app.d
+++ source/app.d
@@ -1,5 +1,7 @@
 import std.stdio;
 
+import lambd.layer;
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
---
+++ source/layer.d
@@ -0,0 +1,27 @@
+/**
+#
+#Copyright (c) 2019 IoTone, Inc. All rights reserved.
+#
+**/
+module lambd.layer;
+
+import std.json;
+import std.string;
+import std.net.curl;
+import std.datetime;
+import std.stdio;
+import std.conv;
+import std.socket;
+import std.regex;
+
+/**
+ * LambdaContext
+ */
+
+/**
+ * Set data from environment
+ */
+
+/**
+ * runHandler
+ */

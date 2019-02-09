0abc27e28a537f03e61c14ee76129ebb32cc831b
David J. Kordsmeier <dkords@gmail.com>
2019-02-08 22:46:56 -0800

    refs #1 Further cleanup
    Need to invoke the layer, get some response data, put into context,
    extecute the handler, and then invoke the response

--- source/layer.d
+++ source/layer.d
@@ -30,6 +30,9 @@ static immutable auto AWS_LAMBDA_FUNCTION_MEMORY_SIZE = "AWS_LAMBDA_FUNCTION_MEM
 static immutable auto AWS_LAMBDA_LOG_GROUP_NAME = "AWS_LAMBDA_LOG_GROUP_NAME";
 static immutable auto AWS_LAMBDA_LOG_STREAM_NAME = "AWS_LAMBDA_LOG_STREAM_NAME";
 static immutable auto AWS_LAMBDA_RUNTIME_API = "AWS_LAMBDA_RUNTIME_API";
+static immutable auto AWS_LAMBDA_RUNTIME_BASE = "/2018-06-01/runtime";
+static immutable auto AWS_LAMBDA_RUNTIME_INVOCATION_NEXT = AWS_LAMBDA_RUNTIME_BASE ~ "/invocation/next";
+static immutable auto AWS_LAMBDA_RUNTIME_INVOCATION_RESPONSE = AWS_LAMBDA_RUNTIME_BASE ~ "/invocation/%s/response";
 // ENV["_X_AMZN_TRACE_ID"] = res.headers["Lambda-Runtime-Trace-Id"]? || ""
 
 /**
@@ -60,12 +63,14 @@ static alias void function(JSONValue evt, LambdaContext ctx) HandlerFunc;
  *
  * General idea: Pass in a HandlerFunc and a Callback
  * Set the environment in the context
+ * Invoke the runtime base
  * Run the handler
- * Post the results back to the lambda
+ * Post the handler response
  *
  */
 JSONValue* runHandler(HandlerFunc handler, CallbackFunc cb) {
   LambdaContext context;
+  string awsLambdaRuntimeAPI;
 
   /**
    * Set data from environment
@@ -75,25 +80,25 @@ JSONValue* runHandler(HandlerFunc handler, CallbackFunc cb) {
   }
 
   if (environment[AWS_LAMBDA_FUNCTION_VERSION] != null) {
-    context.functionName = environment[AWS_LAMBDA_FUNCTION_VERSION];
+    context.functionVersion = environment[AWS_LAMBDA_FUNCTION_VERSION];
   }
 
   if (environment[AWS_LAMBDA_FUNCTION_MEMORY_SIZE] != null) {
-    context.functionName = environment[AWS_LAMBDA_FUNCTION_MEMORY_SIZE];
+    context.memoryLimitInMB = to!int(environment[AWS_LAMBDA_FUNCTION_MEMORY_SIZE]);
   }
 
   if (environment[AWS_LAMBDA_LOG_GROUP_NAME] != null) {
-    context.functionName = environment[AWS_LAMBDA_LOG_GROUP_NAME];
+    context.logGroupName = environment[AWS_LAMBDA_LOG_GROUP_NAME];
   }
 
   if (environment[AWS_LAMBDA_LOG_STREAM_NAME] != null) {
-    context.functionName = environment[AWS_LAMBDA_LOG_STREAM_NAME];
+    context.logStreamName = environment[AWS_LAMBDA_LOG_STREAM_NAME];
   }
 
   if (environment[AWS_LAMBDA_RUNTIME_API] != null) {
-    context.functionName = environment[AWS_LAMBDA_RUNTIME_API];
+    awsLambdaRuntimeAPI = environment[AWS_LAMBDA_RUNTIME_API];
   }
 
-
+  auto resp = get(awsLambdaRuntimeAPI ~ AWS_LAMBDA_RUNTIME_INVOCATION_NEXT);
   return null;
 }
